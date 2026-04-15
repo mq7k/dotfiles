e0760023ed2ea16ac04925bea31d273197b931ce
mq7k <247589825+mq7k@users.noreply.github.com>
2026-04-15 11:58:04 +0200

    feat: 'main' branch supports nvim 0.12

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -39,6 +39,7 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         opts = {
             ensure_installed = {
                 "vim", "lua", "vimdoc",
